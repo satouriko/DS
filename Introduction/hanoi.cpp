c6e17d5bda71965d949347ee340d8896760ce36e
LI JIAHAO <lijiahao@cool2645.com>
2016-09-16 21:05:27 +0800

    hanoi dp

--- Introduction/hanoi.cpp
+++ Introduction/hanoi.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 /*
@@ -14,15 +16,44 @@ using namespace std;
 
 long long cnt = 0;
 
-void hanoi(char from, char by, char to, int n) {
+void hanoi(string from, string by, string to, int n) {
     if(n-1 > 0) hanoi(from, to, by, n-1);
     cout << from << "->" << to << endl; ++cnt;
     if(n-1 > 0) hanoi(by, from, to, n-1);
 }
 
+void hanoi_dp(string from, string by, string to, int n) {
+    /*
+     * 21:abc 11:bac 01:cab
+     * 20:acb 10:bca 00:cba
+     */
+    string v[3][2][n];
+    v[0][0][0] = to + "->" + from + "\n";
+    v[0][1][0] = to + "->" + by + "\n";
+    v[1][0][0] = by + "->"+ from + "\n";
+    v[1][1][0] = by + "->" + to + "\n";
+    v[2][0][0] = from + "->" + by + "\n";
+    v[2][1][0] = from + "->" + to + "\n";
+    for(int k = 1; k < n; ++k) {
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 2; ++j) {
+                v[i][j][k] += v[i][!j][k - 1];
+                v[i][j][k] += v[i][j][0];
+            }
+        v[0][0][k] += v[1][0][k-1];
+        v[0][1][k] += v[2][0][k-1];
+        v[1][0][k] += v[0][0][k-1];
+        v[1][1][k] += v[2][1][k-1];
+        v[2][0][k] += v[0][1][k-1];
+        v[2][1][k] += v[1][1][k-1];
+    }
+    cout << v[2][1][n-1] << endl;
+    cout << "count:" << count(v[2][1][n-1].begin(), v[2][1][n-1].end(), '>') << endl;
+}
+
 int main() {
-    cnt = 0;
-    hanoi('a', 'b', 'c', 6);
-    cout << "count:" << cnt << endl;
+    //cnt = 0;
+    hanoi_dp("a", "b", "c", 6);
+    //cout << "count:" << cnt << endl;
     return 0;
 }
